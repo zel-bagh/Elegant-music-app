26494a81e74dccd8ea6b8e6b63fce8be1ed76f93
zel-bagh <zakaria.baghazaoui@gmail.com>
2023-06-27 06:19:02 +0100

    songCard...

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,8 @@
 const SongCard = () => (
-  <div>SongCard</div>
+  <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className="relative w-full h-56 group">
+      </div>
+    </div>
 );
 
 export default SongCard;
